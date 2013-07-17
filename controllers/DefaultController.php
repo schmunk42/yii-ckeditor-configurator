e4ecf11b5b5e5259fa310a6bf0a81dbf098cbb84
Christopher Stebe <chris@stebe.eu>
2013-07-17 10:35:51 +0200

    Update DefaultController.php
    added action Ckstyles for actual migration (m130716_134508_init_tables)

--- controllers/DefaultController.php
+++ controllers/DefaultController.php
@@ -8,7 +8,33 @@ class DefaultController extends Controller
 	}
 
     public function actionCkstyles() {
-        $this->render('ckstyles');
+        $models = CkeditorStyle::model()->findAll();
+        $style = array();
+
+        foreach ($models AS $model) {
+
+            // clean array %temp foreach new style data set
+            $temp = array();
+
+            $temp['name'] = "'" . $model->name . "'";
+            $temp['element'] = "'" . $model->element . "'";
+
+            // Set attributes->class only when used in this style
+            if ($model->class !== NULL) {
+                $temp['attributes'] = "{'class':'" . $model->class . "'}";
+            }
+            array_push($style, $temp);
+        }
+
+        $json_string = CJSON::encode($style);
+        $styles = str_replace('"', '', $json_string);
+
+        // Remove surrounding [] wich came from array_push
+        $styles_json = substr($styles, 1, -1);
+
+        header("Content-type: text/javascript");
+
+        $this->renderPartial('ckstyles', array('stylesJson' => $styles_json));
     }
 
     public function actionCktemplates() {
