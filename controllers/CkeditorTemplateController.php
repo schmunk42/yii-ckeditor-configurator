10b29cdaa6bccf0224ad0ba46f295843399bfeaa
Christopher Stebe <c.stebe@herzogkommunikation.de>
2013-07-25 10:01:56 +0200

    set $layout to column1 for both controllers

--- controllers/CkeditorTemplateController.php
+++ controllers/CkeditorTemplateController.php
@@ -2,7 +2,7 @@
 
 class CkeditorTemplateController extends Controller
 {
-    #public $layout='//layouts/column2';
+    public $layout='//layouts/column1';
     public $defaultAction = "admin";
     public $scenario = "crud";
 
